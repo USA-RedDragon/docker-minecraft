e2bd0a554a3f00dc2f9eb6c6eeb55f06cc935928
Jacob McSwain <jacob@mcswain.dev>
2026-09-11 22:24:19 -0500

    renovate: add forge

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,16 @@ MEMORY_OPTS=${MEMORY_OPTS:-"-Xms128M -Xmx1G"}
 
 JAVA_OPTS="${MEMORY_OPTS} ${EXTRA_JAVA_OPTS}"
 
-JARPATH=""
+LAUNCH_ARGS=""
 if [ "${MC_VARIANT}" = "paper" ]; then
-    JARPATH="/paper-${PAPER_VERSION}-${PAPER_BUILD}.jar"
+    LAUNCH_ARGS="-jar /paper-${PAPER_VERSION}-${PAPER_BUILD}.jar"
 elif [ "${MC_VARIANT}" = "fabric" ]; then
-    JARPATH="/fabric-${MC_VERSION}-${FABRIC_VERSION}-${INSTALLER_VERSION}.jar"
+    LAUNCH_ARGS="-jar /fabric-${MC_VERSION}-${FABRIC_VERSION}-${INSTALLER_VERSION}.jar"
+elif [ "${MC_VARIANT}" = "forge" ]; then
+    LAUNCH_ARGS="@/forge/libraries/net/minecraftforge/forge/${FORGE_VERSION}/unix_args.txt"
 else
     echo "Unknown variant: ${MC_VARIANT}"
     exit 1
 fi
 
-exec java ${JAVA_OPTS} -jar "${JARPATH}" --nogui
+exec java ${JAVA_OPTS} ${LAUNCH_ARGS} --nogui
